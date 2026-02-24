85451de66f9940188a963efd8accb1d2d42cb81f
sushreesoumya1 <sushreesoumya.mishra@gmail.com>
2026-02-24 05:48:04 +0530

    set a template for the AI to respond

--- src/main/java/com/cohad/springai/controller/PromptController.java
+++ src/main/java/com/cohad/springai/controller/PromptController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/prompt")
+@RequestMapping("/api/prompt/openai")
 public class PromptController {
 
     private final ChatClient openAi;
@@ -17,7 +17,13 @@ public class PromptController {
             @Qualifier("openAiChatClient") ChatClient openAi) {
         this.openAi = openAi;
     }
-    @GetMapping("/openai")
+
+    String promptTemplate = "A customer named {customerName} sent the following message: " +
+            "{customerMessage}. Write a polite and helpful email response addressing the issue. " +
+            "Maintain a professional tone and provide reassurance. Respond as if you're " +
+            "writing the email body only. Don't include subject, signature";
+
+    @GetMapping("/joke")
     public String chat(@RequestParam("message") String message){
 
         return openAi
@@ -26,6 +32,21 @@ public class PromptController {
                         "If user asks anything else then please tell them that " +
                         "you can assist only within your defined scope")
                 .user(message)
+                .call().content();
+    }
+    @GetMapping("/email")
+    public String emailResponse(@RequestParam("customerName") String customerName,
+                        @RequestParam("customerMessage") String customerMessage){
+
+        return openAi
+                .prompt()
+                .system("You specialize in writing email bodies " +
+                        "If user asks anything else then please tell them that " +
+                        "you can assist only within your defined scope.")
+                .user(promptUserSpec ->
+                promptUserSpec.text(promptTemplate)
+                        .param("customerName", customerName)
+                        .param("customerMessage", customerMessage))
                 .call().content();
     }
 }
