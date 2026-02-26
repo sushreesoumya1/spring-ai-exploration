1716d1bc89650363aeb014915397783838e7f95c
sushreesoumya1 <sushreesoumya.mishra@gmail.com>
2026-02-26 06:55:10 +0530

    Chat Memory Added with conversation id

--- src/main/java/com/cohad/springai/controller/ChatMemoryController.java
+++ src/main/java/com/cohad/springai/controller/ChatMemoryController.java
@@ -4,6 +4,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
 
 @RestController
 @RequestMapping("/api/chatMemory")
@@ -15,12 +16,12 @@ public class ChatMemoryController {
         this.chatClient = chatClient;
     }
     @GetMapping("/chat-memory")
-    public ResponseEntity<String> chatMemory(@RequestParam("message") String message) {
-        return ResponseEntity
-                .ok(chatClient.prompt()
-                        .user(message)
-                        .call()
-                        .content());
+    public ResponseEntity<String> chatMemory(@RequestHeader("username") String username
+            ,@RequestParam("message") String message) {
+        return ResponseEntity.ok(chatClient.prompt().user(message).advisors(
+                        advisorSpec -> advisorSpec.param(CONVERSATION_ID, username)
+                )
+                .call().content());
     }
 
 
