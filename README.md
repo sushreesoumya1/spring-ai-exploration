67050dea34a7a69611c6eef5c215f66998a29610
sushreesoumya1 <sushreesoumya.mishra@gmail.com>
2026-03-06 19:12:01 +0530

    Text to MP3

--- README.md
+++ README.md
@@ -13,7 +13,8 @@ This repo is a minimal Spring Boot playground to explore chat models using:
 10) RAG (using RetrievalAugmentationAdvisor)
 11) RAG (with web-search capabilities)
 12) MP3 to Text
-13) Yet To Decide...
+13) Text to MP3
+14) Yet To Decide...
 
 
 ## Prerequisites
--- src/main/java/com/cohad/springai/controller/AudioController.java
+++ src/main/java/com/cohad/springai/controller/AudioController.java
@@ -1,7 +1,10 @@
 package com.cohad.springai.controller;
 
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechResponse;
 import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
@@ -9,7 +12,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/audio")
@@ -38,5 +47,25 @@ public class AudioController {
                 .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.VTT).build()));
         return audioTranscriptionResponse.getResult().getOutput();
     }
+
+    @GetMapping("/speech")
+    String speech(@RequestParam("message") String message) throws IOException {
+        byte[] audioBytes = speechModel.call(message);
+        Path path = Paths.get("output.mp3");
+        Files.write(path, audioBytes);
+        return "MP3 saved successfully to " + path.toAbsolutePath();
+    }
+
+    @GetMapping("/speech-options")
+    String speechWithOptions(@RequestParam("message") String message) throws IOException {
+        TextToSpeechResponse speechResponse = speechModel.call(new TextToSpeechPrompt(message,
+                OpenAiAudioSpeechOptions.builder().voice(OpenAiAudioApi.SpeechRequest.Voice.NOVA)
+                        .speed(2.0)
+                        .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3).build()));
+        Path path = Paths.get("speech-options.mp3");
+        Files.write(path, speechResponse.getResult().getOutput());
+        return "MP3 saved successfully to " + path.toAbsolutePath();
+    }
+
 
 }
